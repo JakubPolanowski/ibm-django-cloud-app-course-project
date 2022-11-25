786ee05db54e7409ac721bff7f5784eca2adba8e
Jakub Polanowski <jakubpol27@gmail.com>
2022-11-25 00:35:30 -0500

    registered question and choices to admin site

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -32,9 +32,15 @@ class LessonAdmin(admin.ModelAdmin):
     list_display = ['title']
 
 
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+
+
 # <HINT> Register Question and Choice models here
 
 admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Instructor)
 admin.site.register(Learner)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
